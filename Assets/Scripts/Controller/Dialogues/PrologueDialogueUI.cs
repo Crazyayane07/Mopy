65cd644abb743c9b3ef3fb985d141949ecac25ce
crazyayane123 <kar.lenczewska@gmail.com>
2019-02-23 13:46:47 +0100

    prolog

---
+++ Assets/Scripts/Controller/Dialogues/PrologueDialogueUI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using Yarn;
+using Yarn.Unity;
+
+namespace MOP.Controller.Dialogues
+{
+    public class PrologueDialogueUI : DialogueUIBehaviour
+    {
+        public TextMeshProUGUI dialogueText;
+
+        public float textSpeed = 0.025f;
+
+        public GameObject dialogueEndStar;
+
+
+        public override IEnumerator RunCommand(Command command)
+        {
+            yield break;
+        }
+
+        public override IEnumerator RunLine(Line line)
+        {
+            dialogueText.gameObject.SetActive(true);
+            var stringBuilder = new StringBuilder();
+
+            foreach (char c in line.text)
+            {
+                stringBuilder.Append(c);
+                dialogueText.text = stringBuilder.ToString();
+                yield return new WaitForSeconds(textSpeed);
+            }
+
+            dialogueEndStar.gameObject.SetActive(true);
+
+            while (Input.anyKeyDown == false)
+            {
+                yield return null;
+            }
+        }
+
+        public override IEnumerator RunOptions(Options optionsCollection, OptionChooser optionChooser)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/View/PrologueView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Yarn.Unity;
+
+namespace MOP.View
+{
+    public class PrologueView : MonoBehaviour
+    {
+        public Sprite[] sprites;
+
+        public Image img;
+
+        [YarnCommand("changeImage")]
+        public void ChangeImage(string id)
+        {
+            Debug.Log(id);
+            img.sprite = sprites[int.Parse(id)];
+        }
+    }
+}
+
