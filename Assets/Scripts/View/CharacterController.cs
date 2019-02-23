d37201190a56b4631b18d81dc57c60434f0aa620
Unknown <mm@maczek.net>
2019-02-23 16:00:39 +0100

    minigame v0.1

---
+++ Assets/Scripts/View/CharacterController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MOP.View
+{
+    public class CharacterController : MonoBehaviour2
+    {
+        public float upForce = 5f;
+        private Rigidbody2D rgb2d;
+
+        private void Start()
+        {
+            rgb2d = GetComponent<Rigidbody2D>();
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKeyDown("space"))
+            {
+                rgb2d.AddForce(new Vector2(0, upForce));
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if(collision.gameObject.tag.Equals("Death"))
+            {
+                Debug.Log("died");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/View/ObstacleView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleView : MonoBehaviour
+{
+    public float speed;
+    public Transform target;
+
+    void Update()
+    {
+
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed *Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target.position) < 0.001f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
